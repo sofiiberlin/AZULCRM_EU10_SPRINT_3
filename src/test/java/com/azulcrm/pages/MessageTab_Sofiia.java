904840b3c84ee5845f73f6c6db643e8cdad2fa1c
sofiiberlin <112969387+sofiiberlin@users.noreply.github.com>
2023-04-10 07:01:18 -1000

    step definitions 4 scenarios created

--- src/test/java/com/azulcrm/pages/MessageTab_Sofiia.java
+++ src/test/java/com/azulcrm/pages/MessageTab_Sofiia.java
@@ -5,47 +5,58 @@ import org.openqa.selenium.support.FindBy;
 
 public class MessageTab_Sofiia extends BasePage {
 
-    @FindBy (xpath = "//a[@title=\"Activity Stream\"]")
-    public WebElement ActivityStream;
-
-    @FindBy (xpath = "//span[@class=\"pagetitle-item\"]")
-    public WebElement MyTask;
-
-    @FindBy (xpath = "//a[@id=\"destLastTab_mention43850789\"]")
+    @FindBy (xpath = "//a[@class='bx-finder-box-tab bx-lm-tab-last bx-finder-box-tab-selected']")
     public WebElement   Recent;
-
-    @FindBy (xpath = "//a[@id=\"destGroupTab_destination3850762\"]")
-    public WebElement MyGroup;
-
-    @FindBy (xpath = "//a[@id=\"destDepartmentTab_mention43850789\"]")
-    public WebElement EmployeesAndDepartment;
-
-    @FindBy (xpath = "//span[.=\"Message\"]")
-    public WebElement MessageTab;
-
-    @FindBy (xpath = "//body[@class=\"vsc-initialized\"]")
+    @FindBy (xpath = "//body[@contenteditable='true']")
     public WebElement MessageField;
+
+    @FindBy (xpath = "//iframe[@class='bx-editor-iframe']")
+    public WebElement IframeMessage;
+
+    @FindBy (xpath = "//span[@class=\"feed-add-post-micro-title\"]")
+    public WebElement SendMessageBox;
 
     @FindBy (xpath = "//span[@class=\"feed-add-post-destination-text\"]")
     public WebElement AllEmployeesSelection;
 
-    @FindBy (xpath = "//a[@class=\"feed-add-destination-link\"]")
+    @FindBy (xpath = "//a[@id=\"bx-destination-tag\"]")
     public WebElement AddMoreButton;
 
-    @FindBy (xpath="//input[@id=\"feed-add-post-destination-input\"]")
+    @FindBy (xpath = "//a[@class='bx-finder-box-tab bx-lm-tab-department']")
+    public WebElement AllEmployeesLink;
+
+    @FindBy (xpath = "(//div[.='marketing7@cybertekschool.com'])[3]")
+    public WebElement Marketing7Emloyee;
+
+    @FindBy (xpath = "(//div[.='marketing7@cybertekschool.com'])[1]")
+    public WebElement marketinginRecent;
+
+    @FindBy (xpath = "(//span[.='marketing7@cybertekschool.com'])[2]")
+    public WebElement checkIfReceipentAdded;
+
+    @FindBy (xpath = "//div[.='helpdesk22@cybertekschool.com']")
+    public WebElement helpdesk22;
+
+    @FindBy (xpath="//input[@id='feed-add-post-destination-input']")
     public WebElement ToSend;
 
     @FindBy (xpath = "//button[@id=\"blog-submit-button-save\"]")
     public WebElement SendButton;
 
+    @FindBy (xpath = "//span[@class=\"popup-window-close-icon\"]")
+    public WebElement popUpWindowCloseIcon;
+
     @FindBy (xpath = "//button[@id=\"blog-submit-button-cancel\"]")
     public WebElement CancelButton;
+
+    @FindBy (xpath = "//span[@class='feed-add-post-del-but']")
+    public WebElement recepientDeleteButton;
 
     @FindBy (xpath = "//div[@class=\"feed-add-error\"]")
     public WebElement ErrorMessage;
 
-    @FindBy (xpath = "//span[@class=\"feed-add-info-text\"]")
-    public WebElement InfoTextError;
+    @FindBy (xpath = "//span[.='The message title is not specified']")
+    public WebElement noMessageTitle;
 
 
 
--- src/test/java/com/azulcrm/step_definitions/Jiranic3_US02_messageTab_Sofiia.java
+++ src/test/java/com/azulcrm/step_definitions/Jiranic3_US02_messageTab_Sofiia.java
@@ -1,111 +1,158 @@
 package com.azulcrm.step_definitions;
 
 import com.azulcrm.pages.LoginPage;
-import com.azulcrm.utilities.ConfigurationReader;
+import com.azulcrm.pages.MessageTab_Sofiia;
+import com.azulcrm.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Jiranic3_US02_messageTab_Sofiia {
 
+    MessageTab_Sofiia MessageTab_Sofiia = new MessageTab_Sofiia();
+    LoginPage loginPage = new LoginPage();
 
-    @When("the user fills in the message title and selects at least one recipient")
-    public void the_user_fills_in_the_message_title_and_selects_at_least_one_recipient() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @When("the user fills in the message title")
+    public void the_user_fills_in_the_message_title() {
+        MessageTab_Sofiia.SendMessageBox.click();
+        Driver.getDriver().switchTo().frame(MessageTab_Sofiia.IframeMessage);
+        MessageTab_Sofiia.MessageField.click();
+        MessageTab_Sofiia.MessageField.sendKeys("Hello");
+        Driver.getDriver().switchTo().parentFrame();
+
     }
+        @And("selects at least one recipient")
+        public void selectsAtLeastOneRecipient() {
+            MessageTab_Sofiia.AddMoreButton.click();
+            MessageTab_Sofiia.AllEmployeesLink.click();
+            MessageTab_Sofiia.Marketing7Emloyee.click();
+            MessageTab_Sofiia.popUpWindowCloseIcon.click();
+
+
+        }
 
     @When("the user clicks on the send button")
     public void the_user_clicks_on_the_send_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        MessageTab_Sofiia.SendButton.click();
+
     }
 
     @Then("the message should be sent successfully")
     public void the_message_should_be_sent_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+      Assert.assertTrue(MessageTab_Sofiia.SendMessageBox.isDisplayed());
+
     }
 
 
     @When("the user selects at least one recipient without filling in the message title")
     public void theUserSelectsAtLeastOneRecipientWithoutFillingInTheMessageTitle() {
+        MessageTab_Sofiia.SendMessageBox.click();
+        MessageTab_Sofiia.AddMoreButton.click();
+        MessageTab_Sofiia.AllEmployeesLink.click();
+        MessageTab_Sofiia.Marketing7Emloyee.click();
+        MessageTab_Sofiia.popUpWindowCloseIcon.click();
 
     }
 
 
     @Then("an error message should be displayed: {string}")
     public void anErrorMessageShouldBeDisplayed(String arg0) {
+        Assert.assertTrue(MessageTab_Sofiia.noMessageTitle.isDisplayed());
     }
 
     @When("the user fills in the message title without selecting any recipient")
     public void theUserFillsInTheMessageTitleWithoutSelectingAnyRecipient() {
-        
-    }
+        MessageTab_Sofiia.SendMessageBox.click();
+        Driver.getDriver().switchTo().frame(MessageTab_Sofiia.IframeMessage);
+        MessageTab_Sofiia.MessageField.click();
+        MessageTab_Sofiia.MessageField.sendKeys("Hello");
+        Driver.getDriver().switchTo().parentFrame();
 
-    @When("the user clicks on the recipient field")
-    public void theUserClicksOnTheRecipientField() {
-        
+
     }
 
     @And("selects at least one recipient from the {string} category")
     public void selectsAtLeastOneRecipientFromTheCategory(String arg0) {
-        
+
+        MessageTab_Sofiia.AddMoreButton.click();
+        MessageTab_Sofiia.Recent.click();
+        MessageTab_Sofiia.marketinginRecent.click();
+        MessageTab_Sofiia.popUpWindowCloseIcon.click();
     }
 
     @Then("the selected recipients should be added to the recipient list")
     public void theSelectedRecipientsShouldBeAddedToTheRecipientList() {
+        Assert.assertTrue(MessageTab_Sofiia.checkIfReceipentAdded.isDisplayed());
         
     }
 
     @Then("the selected group members should be added to the recipient list")
     public void theSelectedGroupMembersShouldBeAddedToTheRecipientList() {
+        // there is no group member field
         
     }
 
     @Then("the selected employees or departments should be added to the recipient list")
     public void theSelectedEmployeesOrDepartmentsShouldBeAddedToTheRecipientList() {
-        
+        Assert.assertTrue(MessageTab_Sofiia.Marketing7Emloyee.isDisplayed());
     }
 
     @When("the user selects multiple recipients from any category")
     public void theUserSelectsMultipleRecipientsFromAnyCategory() {
+        MessageTab_Sofiia.SendMessageBox.click();
+        MessageTab_Sofiia.AddMoreButton.click();
+        MessageTab_Sofiia.AllEmployeesLink.click();
+        MessageTab_Sofiia.Marketing7Emloyee.click();
+        MessageTab_Sofiia.helpdesk22.click();
+        MessageTab_Sofiia.popUpWindowCloseIcon.click();
         
     }
 
     @Then("all selected recipients should be added to the recipient list")
     public void allSelectedRecipientsShouldBeAddedToTheRecipientList() {
+        Assert.assertTrue(MessageTab_Sofiia.Marketing7Emloyee.isDisplayed());
+        Assert.assertTrue(MessageTab_Sofiia.helpdesk22.isDisplayed());
         
     }
 
     @Given("the user has added at least one recipient to the recipient list")
     public void theUserHasAddedAtLeastOneRecipientToTheRecipientList() {
-        
+        MessageTab_Sofiia.SendMessageBox.click();
+        MessageTab_Sofiia.AddMoreButton.click();
+        MessageTab_Sofiia.AllEmployeesLink.click();
+        MessageTab_Sofiia.Marketing7Emloyee.click();
+
     }
 
     @And("the user is on the compose message page")
     public void theUserIsOnTheComposeMessagePage() {
+        MessageTab_Sofiia.popUpWindowCloseIcon.click();
+
         
     }
 
     @When("the user clicks on the delete button next to a recipient")
     public void theUserClicksOnTheDeleteButtonNextToARecipient() {
+        MessageTab_Sofiia.recepientDeleteButton.click();
         
     }
 
     @Then("the recipient should be removed from the recipient list")
     public void theRecipientShouldBeRemovedFromTheRecipientList() {
-        
-        
+        Assert.assertFalse(MessageTab_Sofiia.Marketing7Emloyee.isDisplayed());
     }
 
     @And("the user has not selected any specific recipients")
     public void theUserHasNotSelectedAnySpecificRecipients() {
+    MessageTab_Sofiia.ToSend.clear();
     }
 
     @Then("the message should be sent to all employees")
     public void theMessageShouldBeSentToAllEmployees() {
+        Assert.assertTrue(MessageTab_Sofiia.AllEmployeesSelection.isDisplayed());
         
     }
 
@@ -152,7 +199,16 @@ public class Jiranic3_US02_messageTab_Sofiia {
     @Then("the message should be deleted")
     public void theMessageShouldBeDeleted() {
     }
-}
+
+    @When("user clicks on the recipient field")
+    public void user_clicks_on_the_recipient_field() {
+        MessageTab_Sofiia.SendMessageBox.click();
+
+    }
+
+
+    }
+
 
 
 
